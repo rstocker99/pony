bb4c152124100f3533402160d89b5903a1b77c85
Daniel Lopes <danielvlopes@gmail.com>
2011-03-23 11:56:55 +0800

    add bundler, move from tabs to soft tabs (2 spaces) and add reply_to option

--- lib/pony.rb
+++ lib/pony.rb
@@ -102,138 +102,139 @@ require 'base64'
 
 module Pony
 
-	@@options = {}
+  @@options = {}
 
 # Default options can be set so that they don't have to be repeated.
 # 
 #   Pony.options = { :from => 'noreply@example.com', :via => :smtp, :via_options => { :host => 'smtp.yourserver.com' } }
 #   Pony.mail(:to => 'foo@bar') # Sends mail to foo@bar from noreply@example.com using smtp 
 #   Pony.mail(:from => 'pony@example.com', :to => 'foo@bar') # Sends mail to foo@bar from pony@example.com using smtp
-	def self.options=(value)
-		@@options = value 
-	end
+  def self.options=(value)
+    @@options = value 
+  end
 
-	def self.options()
-		@@options
-	end
+  def self.options()
+    @@options
+  end
 
 # Send an email
 #   Pony.mail(:to => 'you@example.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.')
 #   Pony.mail(:to => 'you@example.com', :html_body => '<h1>Hello there!</h1>', :body => "In case you can't read html, Hello there.")
 #   Pony.mail(:to => 'you@example.com', :cc => 'him@example.com', :from => 'me@example.com', :subject => 'hi', :body => 'Howsit!')
-	def self.mail(options)
-		options = @@options.merge options
-		raise(ArgumentError, ":to is required") unless options[:to]
+  def self.mail(options)
+    options = @@options.merge options
+    raise(ArgumentError, ":to is required") unless options[:to]
 
-		options[:via] = default_delivery_method unless options.has_key?(:via)
-		raise(ArgumentError, ":via must be either smtp or sendmail") unless via_possibilities.include?(options[:via])
+    options[:via] = default_delivery_method unless options.has_key?(:via)
+    raise(ArgumentError, ":via must be either smtp or sendmail") unless via_possibilities.include?(options[:via])
 
-		options = cross_reference_depricated_fields(options)
+    options = cross_reference_depricated_fields(options)
 
-		if options.has_key?(:via) && options[:via] == :sendmail
-			options[:via_options] ||= {}
-			options[:via_options][:location] ||= sendmail_binary
-		end
+    if options.has_key?(:via) && options[:via] == :sendmail
+      options[:via_options] ||= {}
+      options[:via_options][:location] ||= sendmail_binary
+    end
 
-		deliver build_mail(options)
-	end
+    deliver build_mail(options)
+  end
 
-	private
+  private
 
-	def self.cross_reference_depricated_fields(options)
-		if options.has_key?(:smtp)
-			warn depricated_message(:smtp, :via_options)
-			options[:via_options] = options.delete(:smtp)
-		end
+  def self.cross_reference_depricated_fields(options)
+    if options.has_key?(:smtp)
+      warn depricated_message(:smtp, :via_options)
+      options[:via_options] = options.delete(:smtp)
+    end
 
-		# cross-reference pony options to be compatible with keys mail expects
-		{ :host => :address, :user => :user_name, :auth => :authentication, :tls => :enable_starttls_auto }.each do |key, val|
-			if options[:via_options] && options[:via_options].has_key?(key)
-				warn depricated_message(key, val)
-				options[:via_options][val] = options[:via_options].delete(key)
-			end
-		end
+    # cross-reference pony options to be compatible with keys mail expects
+    { :host => :address, :user => :user_name, :auth => :authentication, :tls => :enable_starttls_auto }.each do |key, val|
+      if options[:via_options] && options[:via_options].has_key?(key)
+        warn depricated_message(key, val)
+        options[:via_options][val] = options[:via_options].delete(key)
+      end
+    end
 
-		if options[:content_type] && options[:content_type] =~ /html/ && !options[:html_body]
-			warn depricated_message(:content_type, :html_body)
-			options[:html_body] = options[:body]
-		end
+    if options[:content_type] && options[:content_type] =~ /html/ && !options[:html_body]
+      warn depricated_message(:content_type, :html_body)
+      options[:html_body] = options[:body]
+    end
 
-		return options
-	end
+    return options
+  end
 
-	def self.deliver(mail)
-		mail.deliver!
-	end
+  def self.deliver(mail)
+    mail.deliver!
+  end
 
-	def self.default_delivery_method
-		File.executable?(sendmail_binary) ? :sendmail : :smtp
-	end
+  def self.default_delivery_method
+    File.executable?(sendmail_binary) ? :sendmail : :smtp
+  end
 
-	def self.build_mail(options)
-		mail = Mail.new do
-			to options[:to]
-			from options[:from] || 'pony@unknown'
-			cc options[:cc]
-			bcc options[:bcc]
-			subject options[:subject]
-			date options[:date] || Time.now
-			message_id options[:message_id]
-			sender options[:sender] if options[:sender]
+  def self.build_mail(options)
+    mail = Mail.new do
+      to options[:to]
+      from options[:from] || 'pony@unknown'
+      cc options[:cc]
+      reply_to options[:reply_to]
+      bcc options[:bcc]
+      subject options[:subject]
+      date options[:date] || Time.now
+      message_id options[:message_id]
+      sender options[:sender] if options[:sender]
 
-			if options[:html_body]
-				html_part do
-					content_type 'text/html; charset=UTF-8'
-					body options[:html_body]
-				end
-			end
+      if options[:html_body]
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body options[:html_body]
+        end
+      end
 
-			# If we're using attachments, the body needs to be a separate part. If not,
+      # If we're using attachments, the body needs to be a separate part. If not,
                         # we can just set the body directly.
-			if options[:body] && (options[:html_body] || options[:attachments])
-				text_part do
-					body options[:body]
-				end
-			elsif options[:body]
-				body options[:body]
-			end
+      if options[:body] && (options[:html_body] || options[:attachments])
+        text_part do
+          body options[:body]
+        end
+      elsif options[:body]
+        body options[:body]
+      end
 
-			delivery_method options[:via], (options.has_key?(:via_options) ? options[:via_options] : {})
+      delivery_method options[:via], (options.has_key?(:via_options) ? options[:via_options] : {})
                 end
 
-		(options[:attachments] || []).each do |name, body|
-			# mime-types wants to send these as "quoted-printable"
-			if name =~ /\.xlsx$/
-				mail.attachments[name] = {
-					:content => Base64.encode64(body),
-					:transfer_encoding => :base64
-				}
-			else
-				mail.attachments[name] = body
-			end
-		end
+    (options[:attachments] || []).each do |name, body|
+      # mime-types wants to send these as "quoted-printable"
+      if name =~ /\.xlsx$/
+        mail.attachments[name] = {
+          :content => Base64.encode64(body),
+          :transfer_encoding => :base64
+        }
+      else
+        mail.attachments[name] = body
+      end
+    end
 
-		(options[:headers] ||= {}).each do |key, value|
-			mail[key] = value
-		end
+    (options[:headers] ||= {}).each do |key, value|
+      mail[key] = value
+    end
 
-		mail.charset = options[:charset] if options[:charset] # charset must be set after setting content_type
+    mail.charset = options[:charset] if options[:charset] # charset must be set after setting content_type
 
-		mail
-	end
+    mail
+  end
 
-	def self.sendmail_binary
-		sendmail = `which sendmail`.chomp
-		sendmail.empty? ? '/usr/sbin/sendmail' : sendmail
-	end
+  def self.sendmail_binary
+    sendmail = `which sendmail`.chomp
+    sendmail.empty? ? '/usr/sbin/sendmail' : sendmail
+  end
 
-	def self.via_possibilities
-		%w(sendmail smtp).map{ |x| x.to_sym }
-	end
+  def self.via_possibilities
+    %w(sendmail smtp).map{ |x| x.to_sym }
+  end
 
-	def self.depricated_message(method, alternative)
-		warning_message = "warning: '#{method}' is deprecated"
-		warning_message += "; use '#{alternative}' instead." if alternative
-		return warning_message
-	end
+  def self.depricated_message(method, alternative)
+    warning_message = "warning: '#{method}' is deprecated"
+    warning_message += "; use '#{alternative}' instead." if alternative
+    return warning_message
+  end
 end
